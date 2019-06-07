ec9f53135cd32d26a52c1259dbefb88a48902103
Arun C S <acspayyeri@gmail.com>
2019-06-07 20:53:19 +0530

    initial commit

---
+++ code.js
@@ -0,0 +1,16 @@
+window.addEventListener("load",function(){
+  
+})
+
+function handleSidePannelOpenButtonClick(){
+  debugger;
+  let sidePannelWrapper = document.getElementById("idSidePannelWrapper")
+  sidePannelWrapper.style.width = '250px'
+  
+}
+
+function handleSidePannelContentMouseOut(){
+  debugger
+  let sidePannelWrapper = document.getElementById("idSidePannelWrapper")
+  sidePannelWrapper.style.width = '0px' 
+}
